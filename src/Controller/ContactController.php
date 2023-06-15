c28363f5767b6467c75559203bea34263caa39b9
Juhász Csaba József <h870697@stud.u-szeged.hu>
2023-06-15 18:10:00 +0200

    #4 Add Contact Form and DB saving

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -4,15 +4,30 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Contact;
+use App\Form\ContactType;
 
 class ContactController extends AbstractController
 {
     #[Route('/', name: 'app_contact')]
-    public function index(): Response
+    public function index(Request $request, EntityManagerInterface $entityManager): Response
     {
+        $contact = new Contact();
+        $form = $this->createForm(ContactType::class, $contact);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $contact = $form->getData();
+            $entityManager->persist($contact);
+            $entityManager->flush();
+            return $this->redirectToRoute('app_contact');
+        }
+
         return $this->render('contact/index.html.twig', [
-            'controller_name' => 'ContactController',
+            'form' => $form,
         ]);
     }
 }
